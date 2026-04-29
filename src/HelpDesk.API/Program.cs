c938e6d40f25b6d22437489c9eeb6fc499c9c583
Raphael Ribeiro <raphaelfribeiro@usp.br>
2026-04-29 19:34:41 -0300

    Incluindo Swagger ao projeto HelpDesk.API
    - Adicionando o pacote Swashbuckle.AspNetCore ao projeto HelpDesk.API para suporte ao Swagger.
    - Incluindo o uso do Swagger em ambiente de desenvolvimento na classe principal da solução.

--- src/HelpDesk.API/Program.cs
+++ src/HelpDesk.API/Program.cs
@@ -5,9 +5,11 @@ using HelpDesk.Infrastructure.Data;
 using HelpDesk.Infrastructure.Queries;
 using HelpDesk.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -15,17 +17,49 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         builder.Configuration
         .GetConnectionString("Default")));
 
-
 builder.Services.AddScoped<ITicketRepository, TicketRepository>();
 builder.Services.AddScoped<TicketService>();
 builder.Services.AddScoped<TicketQuery>(sp =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
-    return new TicketQuery(
-        config.GetConnectionString("Default"));
+    var connectionString = config.GetConnectionString("Default")
+        ?? throw new InvalidOperationException("Connection string 'Default' was not found.");
+
+    return new TicketQuery(connectionString);
+});
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var tenantSecurityScheme = new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "X-Tenant-Id",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "apiKey",
+        Description = "Tenant Id header"
+    };
+
+    options.AddSecurityDefinition("TenantId", tenantSecurityScheme);
+    options.AddSecurityRequirement(_ => new OpenApiSecurityRequirement
+    {
+        { new OpenApiSecuritySchemeReference("TenantId", null, null), new List<string>() }
+    });
 });
 
 var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.UseMiddleware<TenantMiddleware>();
 
