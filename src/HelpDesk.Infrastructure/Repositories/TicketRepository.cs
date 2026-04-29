a04393eac397c5055bcdf08162707f376575c7b7
Raphael Ribeiro <raphaelfribeiro@usp.br>
2026-04-29 12:35:36 -0300

    Criando Repository EF
    - O Repository EF (Entity Framework Repository) foi criado para abstrair o acesso ao banco de dados, organizando as operações de leitura e escrita em um lugar separado da lógica de negócio. Servindo como camada intermediária entre o DbContext e a aplicação.
    
    Obs: dessa forma o Controller chama repository, Repository acessa banco e o Código fica organizado.

---
+++ src/HelpDesk.Infrastructure/Repositories/TicketRepository.cs
@@ -0,0 +1,33 @@
+﻿using HelpDesk.Domain.Entities;
+using HelpDesk.Domain.Repositories;
+using HelpDesk.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk.Infrastructure.Repositories;
+
+public class TicketRepository : ITicketRepository
+{
+    private readonly AppDbContext _context;
+
+    public TicketRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Ticket ticket)
+    {
+        await _context.Tickets.AddAsync(ticket);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Ticket?> GetByIdAsync(Guid id)
+    {
+        return await _context.Tickets
+            .FirstOrDefaultAsync(t => t.Id == id);
+    }
+
+    public async Task<IEnumerable<Ticket>> GetAllAsync()
+    {
+        return await _context.Tickets.ToListAsync();
+    }
+}
