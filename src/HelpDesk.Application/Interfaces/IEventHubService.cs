d381f3f79964bdfee84935b62abf0c5a1576ed42
Raphael Ribeiro <raphaelfribeiro@usp.br>
2026-05-04 22:12:19 -0300

    Adicionando o Azure event hub + auditoria distribuida
    - Criando Event streaming com Event Hub.
    - Adicionando dois pacotes no projeto HelpDesk.Infrastructure: Azure.Messaging.EventHubs e Azure.Messaging.EventHubs.Producer.
    - Criando producer (event hub).
    - Adicionando config do EventHub no appsettings.json e registro no DI.
    - Publicando evento de auditoria.
    - Criando audit worker.
    - Implementando processador do event hub.
    - Incluindo config do worker no appsettings.json.

---
+++ src/HelpDesk.Application/Interfaces/IEventHubService.cs
@@ -0,0 +1,6 @@
+﻿namespace HelpDesk.Application.Interfaces;
+
+public interface IEventHubService
+{
+    Task PublishAsync<T>(T data);
+}
---
+++ src/HelpDesk.Infrastructure/Services/EventHubService.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using HelpDesk.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+
+namespace HelpDesk.Infrastructure.Services;
+
+public class EventHubService : IEventHubService
+{
+    private readonly EventHubProducerClient _producer;
+    private readonly string _connectionString;
+    private readonly string _hubName;
+
+    public EventHubService(IConfiguration config)
+    {
+        _connectionString = config["EventHub:ConnectionString"];
+        _hubName = config["EventHub:HubName"];
+
+        _producer = new EventHubProducerClient(_connectionString, _hubName);
+    }
+
+    public async Task PublishAsync<T>(T data)
+    {
+        using var batch = await _producer.CreateBatchAsync();
+
+        var json = JsonSerializer.Serialize(data);
+        batch.TryAdd(new EventData(Encoding.UTF8.GetBytes(json)));
+
+        await _producer.SendAsync(batch);
+    }
+}
