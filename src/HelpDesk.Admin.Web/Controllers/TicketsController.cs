4589d452f82a2b6270c52e3a14511e5c273aab50
Raphael Ribeiro <raphaelfribeiro@usp.br>
2026-05-05 16:59:41 -0300

    Incluindo ASP.NET Core MVC
    - Incluindo Dashboard admin

---
+++ src/HelpDesk.Admin.Web/Controllers/TicketsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelpDesk.Admin.Web.Services;
+
+namespace HelpDesk.Admin.Web.Controllers;
+
+public class TicketsController : Controller
+{
+    private readonly TicketApiService _service;
+
+    public TicketsController(TicketApiService service)
+    {
+        _service = service;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var tickets = await _service.GetTicketsAsync();
+        return View(tickets);
+    }
+}
---
+++ src/HelpDesk.Admin.Web/Services/TicketApiService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using HelpDesk.Admin.Web.Models;
+
+namespace HelpDesk.Admin.Web.Services;
+
+public class TicketApiService
+{
+    private readonly HttpClient _http;
+
+    public TicketApiService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<List<TicketViewModel>> GetTicketsAsync()
+    {
+        var response = await _http.GetFromJsonAsync<List<TicketViewModel>>("/api/tickets");
+        return response;
+    }
+}
