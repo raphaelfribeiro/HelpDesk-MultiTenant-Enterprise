851c214ab1dd657438df09d604da5edd33b83c45
Raphael Ribeiro <raphaelfribeiro@usp.br>
2026-05-05 16:59:41 -0300

    Incluindo ASP.NET Core MVC
    - Incluindo Dashboard admin

---
+++ src/HelpDesk.Admin.Web/Models/TicketViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpDesk.Admin.Web.Models;
+
+public class TicketViewModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ src/HelpDesk.Admin.Web/Views/Tickets/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelpDesk.Admin.Web.Views.Tickets
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
